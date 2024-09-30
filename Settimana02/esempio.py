b4088040478b4a1acb85d0be3c383b3a76d2c391
Fulvio Corno <fulvio.corno@polito.it>
2024-09-30 18:08:46 +0200

    esempi su variabili

---
+++ Settimana02/esempio.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+base = 5
+altezza = 3
+print(base) # stampa di prova
+area_triangolo = base * altezza / 2
+# calcolo l'area del triangolo
+# 
+# at = base * altezza / 2
+print(area_triangolo)
+base = 2
+print(area_triangolo)
+
+base = base + 1
+print(base)
+
+# scrivi un programma che calcoli l'area
+# del triangolo, e poi visualizzi
+# il risultato
+
+"""
+posso scrivere cio che voglio
+anche su piu righe
+e verra ignorato
+"""
+
+'''
+ciao
+'''
+lato = sqrt(area_triangolo)
