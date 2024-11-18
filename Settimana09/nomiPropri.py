08830a2d0af45665c1c9f09df76e111fec614c45
bardini <roberta.bardini@polito.it>
2024-11-18 15:02:18 +0100

    +esericizi

---
+++ Settimana09/nomiPropri.py
@@ -0,0 +1,24 @@
+"""
+
+Scrivere un codice Python che, dato un testo contenente parole e caratteri vari, esegua:
+
+   - Identificare tutte le parole composte da una sola parola che iniziano con una lettera maiuscola seguita da lettere minuscole.
+
+   - Assicurarsi che ogni nome estratto abbia la prima lettera maiuscola e le restanti minuscole.
+
+   - Generare una nuova lista con gli stessi nomi scritti al contrario.
+   - Ogni nome invertito deve iniziare con una lettera maiuscola seguito da lettere minuscole.
+Esempio:
+
+Dato il testo:
+
+
+testo = "Ciao a tutti, mi chiamo Mario e lui è Luigi. Paolo è mio fratello, Giovanni è mio cugino."
+
+
+Il programma deve produrre:
+
+
+nomi = ['Mario', 'Luigi', 'Paolo', 'Giovanni']
+rovescio = ['Oiram', 'Iugil', 'Olaop', 'Innavoig']
+"""
