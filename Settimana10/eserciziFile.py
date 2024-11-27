c78233f03c4f4a2de815ab289109aaa32be898c2
bardini <roberta.bardini@polito.it>
2024-11-27 16:57:28 +0100

    +lettura righe con while

--- Settimana10/eserciziFile.py
+++ Settimana10/eserciziFile.py
@@ -10,26 +10,41 @@ infile = open("datiNumerici.txt", "r", encoding="UTF-8")
 # un_carattere = infile.read(3)
 # print(un_carattere)
 
-# Legge una linea dal file e rimuove il carattere di nuova linea '\n' alla fine
+# Legge una riga dal file e rimuove il carattere di nuova linea '\n' alla fine
 # una_riga = infile.readline().rstrip()
 # print(una_riga)
 
-# Legge tutte le linee rimanenti dal file e le memorizza in una lista
+# Legge tutte le righe rimanenti dal file e le memorizza in una lista
 # tutte_le_righe = infile.readlines()
 # print(tutte_le_righe)
 
 # Inizializza una variabile per la somma
 somma = 0
 
-# Itera attraverso ogni linea rimanente nel file
-for line in infile:
-    # Rimuove il carattere di nuova linea '\n' alla fine e converte la linea in un intero
+# Utilizzando il for
+# Itera attraverso ogni riga nel file
+#for line in infile:
+#    # Rimuove il carattere di nuova linea '\n' alla fine e converte la linea in un intero
+#    n = int(line.rstrip())
+    # Aggiunge il numero alla somma totale
+#    somma = somma + n
+
+# Utilizzando il while
+# Legge la prima riga del file
+line = infile.readline()
+
+# Itera attraverso ciascuna riga utilizzando readline()
+while line!="":
+    # Rimuove il carattere di nuova linea '\n' alla fine e converte la riga in un intero
     n = int(line.rstrip())
     # Aggiunge il numero alla somma totale
-    somma = somma + n
+    somma += n
+    # Legge la linea successiva
+    line = infile.readline()
 
 # Stampa la somma totale dei numeri nel file
 print(somma)
+
 
 # Chiude il file
 infile.close()
