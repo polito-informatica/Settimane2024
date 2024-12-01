9f2d21e325cbe03406e65676fba341d35ab55845
Fulvio Corno <fulvio.corno@polito.it>
2024-12-01 19:11:23 +0100

    esercizi di esempio per Settimana 11

---
+++ Settimana11/poesia/parole_lunghe.py
@@ -0,0 +1,8 @@
+# Sia dato il file di testo `apelle.txt` che contiene una nota filastrocca,
+# si vuole determinare:
+# a) quale sia la statistica della lunghezza delle parole (quante parole di 1 lettera, quante di 2, e così via),
+#    calcolate escludendo gli eventuali segni di punteggiatura.
+# b) ri-creare un secondo file (il cui nome sia lo stesso del file precedente, con il prefisso `lunghe_`), che contenga
+#    solamente le parole più lunghe di 3 caratteri (ma ricopiando spazi, punteggiatura e fine linea)
+
+NOME_FILE_POESIA = 'apelle.txt'
---
+++ Settimana11/voti/media_pesata.py
@@ -0,0 +1,7 @@
+# Dato il file `voti.txt`, che contiene i voti che uno studente ha acquisito nei vari esami superati,
+# si calcoli il valore della "media pesata" per lo studente.
+#
+# Ciascuna riga del file "voti.txt" contiene 3 campi, separati da spazi: il nome del corso (si assuma privo di spazi),
+# il numero di CFU (numero intero positivo), il voto acquisito (numero intero tra 18 e 30)
+
+NOME_FILE_VOTI = 'voti.txt'
